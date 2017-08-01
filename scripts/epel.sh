24ac928d74261819ffda97e0b30e058db3704d00
idan mishor <idan.mishor@sap.com>
2017-08-01 19:24:49 +0300

    fixing yum update to not update kernel

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -3,4 +3,5 @@
 sudo rpm --import https://fedoraproject.org/static/0608B895.txt
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 # yum install epel-release
-sudo yum -y update --skip-broken
+#sudo yum --releasever=6.7 -y update --skip-broken
+sudo yum -y --exclude=kernel\* update --skip-broken
