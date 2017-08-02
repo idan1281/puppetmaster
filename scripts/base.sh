fbe2b09a58c4c2f3d5b93b7651763f095057af85
idan mishor <idan.mishor@sap.com>
2017-08-02 08:47:06 +0300

    disabling ipv6 on boot

--- scripts/base.sh
+++ scripts/base.sh
@@ -5,4 +5,11 @@ sudo yum -y install wget curl openssh-server
 sudo yum -y install ca-certificates
 
 # Make ssh faster by not waiting on DNS
-#echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "UseDNS no" >> /etc/ssh/sshd_config
+
+# Disabling ipv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+# Restarting Network Service
+sudo service network restart
