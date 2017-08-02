083d7854ea62a0fe75a1fecd48412ca197ad2bd8
idan mishor <idan.mishor@sap.com>
2017-08-02 14:37:32 +0300

    disabling IPV6 in the kickstart instead of using a packer provisioner'

--- scripts/base.sh
+++ scripts/base.sh
@@ -8,8 +8,8 @@ sudo yum -y install ca-certificates
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
 # Disabling ipv6
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+#echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
 # Restarting Network Service
 sudo service network restart
